99f6aee7fe06d815fa30097ab7e2a10d11c91b70
Kyle Behrensen <ptcasual@gmail.com>
2024-01-04 13:16:39 -0500

    Finish refactoring into functional code

--- GearRatios.cpp
+++ GearRatios.cpp
@@ -6,28 +6,26 @@
 #include <string>
 
 const std::string numSearch = "0123456789";
-const std::string symbolSearch = "*$&%@+/#=-";
 
 void setLines(std::string &Line1, std::string &Line2, std::string &Line3, std::string input) {
     Line1 = Line2;
     Line2 = Line3;
-    Line3 = input;
+    if (input == "")
+        std::fill(std::begin(Line3), std::end(Line3), ' ');
+    else
+        Line3 = input;
 }
 
-int compareNotFromBeginning(size_t &found, std::string Line2, std::string Line1, std::string Line3) {
-    size_t beginPos = found;
+int lookForSymbols(size_t &found, std::string Line2, std::string Line1, std::string Line3) {
+    const std::string symbolSearch = "*$&%@+/#=-";
     std::string buildingNum = "";
-    int searchLength = 0;
     int partNum = 0;
 
-    buildingNum = Line2.substr(beginPos, Line2.find_first_of("." + symbolSearch, beginPos) - beginPos);
+    buildingNum = Line2.substr(found, Line2.find_first_of("." + symbolSearch, found) - found);
 
-    if (beginPos == 0)
-        searchLength = buildingNum.length() + 1;
-    else
-        searchLength = buildingNum.length() + 2;
+    int searchLength = (found == 0) ? buildingNum.length() + 1 : buildingNum.length() + 2;
 
-    size_t subBeginPos = std::max(0, static_cast<int>(beginPos - 1));
+    size_t subBeginPos = std::max(0, static_cast<int>(found - 1));
 
     if (Line1.substr(std::min(subBeginPos, Line1.length()), searchLength).find_first_of(symbolSearch) != std::string::npos ||
         Line3.substr(subBeginPos, searchLength).find_first_of(symbolSearch) != std::string::npos ||
@@ -36,81 +34,48 @@ int compareNotFromBeginning(size_t &found, std::string Line2, std::string Line1,
 
         partNum = std::stoi(buildingNum);
     }
-    found = Line2.find_first_of(numSearch, beginPos + searchLength - 1);
+    found = Line2.find_first_of(numSearch, found + searchLength - 1);
 
     return partNum;
 }
 
-int main()
-{
+int searchForNum(std::string Line1, std::string Line2, std::string Line3) {
+    size_t found = 0;
+    int partNums = 0;
+
+    found = Line2.find_first_of(numSearch);
+
+    while (found != std::string::npos)
+        partNums += lookForSymbols(found, Line2, Line1, Line3);
+
+    return partNums;
+}
+
+int handleLastLine(std::string &Line1, std::string &Line2, std::string &Line3) {
+    setLines(Line1, Line2, Line3, "");
+    return searchForNum(Line1, Line2, Line3);
+}
+
+int processSchematic() {
     std::string Line1(140, ' ');
     std::string Line2(140, ' ');
     std::string Line3(140, ' ');
-    std::string input = "";      
+    std::string input = "";
     int partNumSum = 0;
-    int partNum = 0;
-    size_t found = 0;
 
     std::ifstream Schematic("Schematic.txt");
 
     while (std::getline(Schematic, input)) {
         setLines(Line1, Line2, Line3, input);
-
-        found = 0;
-        partNum = 0;        
-
-        if (Line2 == "")
-            continue;
-        else {
-            found = Line2.find_first_of(numSearch);
-
-            while (found != std::string::npos) {
-                int searchLength = 0;
-                std::string buildingNum = "";
-                partNumSum += compareNotFromBeginning(found, Line2, Line1, Line3);            
-            }
-        }
+        partNumSum += searchForNum(Line1, Line2, Line3);
     }
 
-    //Read last line of file
-    found = Line3.find_first_of(numSearch);
+    return partNumSum + handleLastLine(Line1, Line2, Line3);
+}
 
-    while (found != std::string::npos) {
-        int searchLength = 0;
-        std::string buildingNum = "";
-        // If we're starting our compares at the beginning of the line
-        if (found == 0) {
-            while (found != std::string::npos && Line3.find('.') > found) {
-                buildingNum += Line3[found];
-                found = Line3.find_first_of(numSearch, found + 1);
-            }
-            searchLength = buildingNum.length() + 1;
-            if (Line2.substr(0, searchLength).find_first_of(symbolSearch) != std::string::npos) {
-                partNum = std::stoi(buildingNum);
-                partNumSum += partNum;
-            }
-        }
-        else {
-            size_t beginPos = found;
-            
-            buildingNum = Line3.substr(beginPos, Line3.find_first_of("." + symbolSearch, beginPos) - beginPos);
-
-            searchLength = buildingNum.length() + 2;
-
-            size_t subBeginPos = std::max(0, static_cast<int>(beginPos - 1));
-            
-            if (Line2.substr(subBeginPos, searchLength).find_first_of(symbolSearch) != std::string::npos ||
-                Line3.substr(subBeginPos, 1).find_first_of(symbolSearch) != std::string::npos ||
-                Line3.substr(subBeginPos + searchLength - 1, 1).find_first_of(symbolSearch) != std::string::npos) {
-
-                partNum = std::stoi(buildingNum);
-                partNumSum += partNum;
-            }
-            found = Line3.find_first_of(numSearch, beginPos + searchLength - 1);
-        }
-    }
-
-    std::cout << "The sum of the part numbers in the engine schematic is " << partNumSum << '!';
+int main()
+{   
+    std::cout << "The sum of the part numbers in the engine schematic is " << processSchematic() << '!';
 }
 
 
